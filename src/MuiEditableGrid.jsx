697c2cd6a1d81ca9fd8297497995bb0a9a7837e6
SlavenB <slaven.bunijevac@gmail.com>
2023-01-13 00:40:51 +0100

    add handle addRow functionality

--- src/MuiEditableGrid.jsx
+++ src/MuiEditableGrid.jsx
@@ -1,23 +1,43 @@
+import { useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-  { field: 'broj', headerName: 'broj', type: 'number', width: 50, headerAlign: 'center' },
-  { field: 'Konto', headerName: 'Konto', type: 'string', width: 130, editable: true, headerAlign: 'left' },
-  { field: 'Duguje', headerName: 'Duguje', type: 'number', width: 130, editable: true, headerAlign: 'left' },
-  { field: 'Potražuje', headerName: 'Potražuje', type: 'number', width: 130, editable: true, headerAlign: 'left' },
-  { field: 'Datum', headerName: 'Datum', type: 'date', width: 130, editable: true, headerAlign: 'left' },
-];
-
-const rows = [
-  { id: 1, broj: 12, Konto: 'malo li je', Duguje: 50, Potražuje: 60, Datum: new Date() },
-  { id: 2, broj: 13, Konto: 'malo li je', Duguje: 50, Potražuje: 60, Datum: new Date() },
-  { id: 3, broj: 14, Konto: 'malo li je', Duguje: 50, Potražuje: 60, Datum: new Date() },
-];
-
 const MuiEditableGrid = () => {
+
+  const [ rows, setRows ] = useState([
+    { id: 1, broj: 12, Konto: 'malo li je', Duguje: 50, Potražuje: 60, Datum: new Date() },
+    { id: 2, broj: 13, Konto: 'malo li je', Duguje: 50, Potražuje: 60, Datum: new Date() },
+    { id: 3, broj: 14, Konto: 'malo li je', Duguje: 50, Potražuje: 60, Datum: new Date() },
+  ])
+
+  const columns = [
+    { field: 'broj', headerName: 'broj', type: 'number', width: 50, headerAlign: 'center' },
+    { field: 'Konto', headerName: 'Konto', type: 'string', width: 130, editable: true, headerAlign: 'left' },
+    { field: 'Duguje', headerName: 'Duguje', type: 'number', width: 130, editable: true, headerAlign: 'left' },
+    { field: 'Potražuje', headerName: 'Potražuje', type: 'number', width: 130, editable: true, headerAlign: 'left' },
+    { field: 'Datum', headerName: 'Datum', type: 'date', width: 130, editable: true, headerAlign: 'left' },
+  ];
+
+  const handleAddRow = () => {
+    const newBlankRow = {
+      id: Math.floor(Math.random() * 10000000),
+      broj: 0,
+      Konto: '',
+      Duguje: 0,
+      Potražuje: 0,
+      Datum: new Date()
+    }
+    
+    setRows(prevRows => [ ...prevRows, newBlankRow ] )
+  }
+  
   return (
     <div className="editable-grid">
-      <button className="add-grid-row-btn">Add row</button>
+      <button 
+        className="add-grid-row-btn"
+        onClick={handleAddRow}
+      >
+        Add row
+      </button>
       <DataGrid getCellClassName={params => 'grid-cell'} className="data-grid" rows={rows} columns={columns} />
     </div>
   )
